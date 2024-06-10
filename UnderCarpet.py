3102cbe24c6bbd101bf069e24ab064f2583697a0
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-06-10 12:13:30 -0300

    Update UnderCarpet.py

--- UnderCarpet.py
+++ UnderCarpet.py
@@ -4,6 +4,17 @@ from Crypto.Cipher import AES #  cryption
 from Crypto.Util.Padding import pad, unpad #
 
 # gerenation key
-key = get_random_bytes(32)
+# generate_key = get_random_bytes(32)
+# print (key)
 
-print (key)
+salt = b'\x08\xa8\x95r\xa8W-\xacY\xf2\xab\xb8\x8d{Di\xa0\xb67>\xb7-lf,Y\xd1\xcd\xc3D\xfdY'
+password = "Nala00110111#"
+
+key = PBKDF2(password, salt, dkLen=32)
+
+message = b"i dont know"
+
+cipher = AES.new(key, AES.MODE_CBC)
+ciphered_data = cipher.encrypt(pad(message, AES.block_size))
+
+print (ciphered_data)
