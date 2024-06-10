80d18a95ecf7212a222a8bab940cbadd6b80d048
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-06-10 13:02:50 -0300

    Update UnderCarpet.py

--- UnderCarpet.py
+++ UnderCarpet.py
@@ -1,30 +1,18 @@
-from Crypto.Random import get_random_bytes # generate random key
-from Crypto.Protocol.KDF import PBKDF2 # brute-force protection
-from Crypto.Cipher import AES #  cryption
-from Crypto.Util.Padding import pad, unpad #
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
-# gerenation key
-# generate_key = get_random_bytes(32)
-# print (key)
+def encrypt_file(file_path, key):
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    with open(file_path, 'rb') as f:
+        plaintext = f.read()
+    ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+    
+    with open(file_path + ".enc", 'wb') as f:
+        f.write(nonce)
+        f.write(tag)
+        f.write(ciphertext)
 
-salt = b'\x08\xa8\x95r\xa8W-\xacY\xf2\xab\xb8\x8d{Di\xa0\xb67>\xb7-lf,Y\xd1\xcd\xc3D\xfdY'
-password = "Nala00110111#"
-
-key = PBKDF2(password, salt, dkLen=32)
-
-message = b"i dont know"
-
-cipher = AES.new(key, AES.MODE_CBC)
-ciphered_data = cipher.encrypt(pad(message, AES.block_size))
-
-with open('encrypted.bin', 'wb') as f:
-    f.write(cipher.iv)
-    f.write(ciphered_data)
-
-with open('encrypted.bin', 'rb') as f:
-    iv = f.read(16)
-    decrypt_data = f.read()
-
-cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-original = unpad(cipher.decrypt(decrypt_data), AES.block_size)
-print (original)
+# Gerar uma chave de 256 bits
+key = get_random_bytes(32)
+encrypt_file('example.txt', key)
