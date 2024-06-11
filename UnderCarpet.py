631046528f1a4e9b0b9f350e0a1ccc3f9c47935d
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-06-11 15:50:36 -0300

    Update UnderCarpet.py

--- UnderCarpet.py
+++ UnderCarpet.py
@@ -3,7 +3,13 @@ from commands import *
 
 
 # Encrypt all files in the directory
-encrypt_directory(directory, key)
+#encrypt_directory(directory, key)
+
+# Decrypt all files in the directory
+#decrypt_directory(directory, key)
 
 # UnderCarpet Screen
 #screen(screen)
+
+# powershell command
+#powershell(powershell)
